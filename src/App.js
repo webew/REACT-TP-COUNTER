06e6590727886ba460e2c1c343f8d1357a7e634e
webew <fillion.bertrand@gmail.com>
2021-01-12 20:36:21 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Counters from "./components/Counters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counters: [
+                { id: 1, value: 2 },
+                { id: 2, value: 0 },
+                { id: 3, value: 1 },
+                { id: 4, value: 0 },
+            ],
+        };
+    }
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index].value++;
+        this.setState({ counters });
+    };
+    handleDecrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index].value--;
+        this.setState({ counters });
+    };
+
+    handleDelete = (id) => {
+        const counters = this.state.counters.filter((c) => c.id !== id);
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map((c) => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+    handleRestart = () => {
+        this.setState({
+            counters: [
+                { id: 1, value: 2 },
+                { id: 2, value: 0 },
+                { id: 3, value: 1 },
+                { id: 4, value: 0 },
+            ],
+        });
+    };
+    handleAddCounter = () => {
+        const maxId = this.getMaxId();
+        this.setState({
+            counters: [...this.state.counters, { id: maxId + 1, value: 0 }],
+        });
+    };
+    getMaxId = () => {
+        let max = 0;
+        this.state.counters.forEach((item) => {
+            max = Math.max(item.id, max);
+        });
+        return max;
+    };
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar
+                    totalCounters={
+                        this.state.counters.filter((c) => c.value > 0).length
+                    }
+                />
+                <main className="container-fluid">
+                    <Counters
+                        counters={this.state.counters}
+                        onReset={this.handleReset}
+                        onRestart={this.handleRestart}
+                        onAddCounter={this.handleAddCounter}
+                        onIncrement={this.handleIncrement}
+                        onDecrement={this.handleDecrement}
+                        onDelete={this.handleDelete}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
